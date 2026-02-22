01ffa67855be9e8b767ddce7d625084afd994538
camilohl24 <juancamilohernandezhl@gmail.com>
2026-02-22 14:59:39 -0500

    add employee class

---
+++ OPPConcepts/OPPConcepts.Backend/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPPConcepts.Backend;
+
+public abstract class Employee
+{
+    protected Employee()
+    {
+    }
+
+    protected Employee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        IsActive = isActive;
+        BornDate = bornDate;
+        HireDate = hireDate;
+    }
+
+    // Constructors
+
+    // properties
+    // si sale raya  verde se le quita poniendo el null en caso que se pueda dejar por defecto nulo y con admiracionsi ni se puede
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public Date BornDate { get; set; } = null!;
+    public Date HireDate { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"{Id}\t{FirstName} {LastName}\n\t" +
+               $"Value to pay :  {GetValueToPay()}";
+    }
+    public abstract decimal GetValueToPay();
+    
+
+
+}
